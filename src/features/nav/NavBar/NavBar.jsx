542d4e786b146713d014f271f72e76b6da688d97
Charles Burch <charlesburch@CBMacBook-Pro.local>
2018-11-21 19:10:56 -0600

    import navbar

--- src/features/nav/NavBar/NavBar.jsx
+++ src/features/nav/NavBar/NavBar.jsx
@@ -3,24 +3,22 @@ import { Menu, Container, Button } from 'semantic-ui-react'
 
 class NavBar extends Component {
   render() {
-    return (
-      <Menu inverted fixed="top">
+    return <Menu inverted fixed="top">
         <Container>
           <Menu.Item header>
             <img src="assets/logo.png" alt="logo" />
             Reactin
-                  </Menu.Item>
+          </Menu.Item>
           <Menu.Item name="Events" />
           <Menu.Item>
             <Button floated="right" positive inverted content="Create Event" />
           </Menu.Item>
           <Menu.Item position="right">
             <Button basic inverted content="Login" />
-            <Button basic inverted content="Sign Out" style={{ marginLeft: '0.5em' }} />
+            <Button basic inverted content="Sign Out" style={{ marginLeft: "0.5em" }} />
           </Menu.Item>
         </Container>
-      </Menu>
-    )
+      </Menu>;
   }
 }
 
