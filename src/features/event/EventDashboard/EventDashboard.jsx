c148fe4ccc3ea6048eb0681b46fbba296e31caa9
Charles Burch <charlesburch@CBMacBook-Pro.local>
2018-11-22 11:51:30 -0600

    move eventform to the dashboard component

--- src/features/event/EventDashboard/EventDashboard.jsx
+++ src/features/event/EventDashboard/EventDashboard.jsx
@@ -1,6 +1,9 @@
 import React, { Component } from 'react'
 import { Grid } from 'semantic-ui-react'
+
 import EventList from "../EventList/EventList";
+import EventForm from "../EventForm/EventForm";
+
 
 
 class EventDashboard extends Component {
@@ -12,7 +15,7 @@ class EventDashboard extends Component {
                 <EventList/>
                 </Grid.Column>
                 <Grid.Column width={6}>
-                    <h2>Right Column</h2>
+                    <EventForm/>
                 </Grid.Column>
           </Grid>
       </div>
